a4938039b4e4b2d37344699780254eca86676e64
ccc <93768075@qq.com>
2018-08-28 11:12:25 +0800

    优化代码，爬取效率提升200%

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 #请求代理
 PROXIES = {
     'http': 'socks5://127.0.0.1:1086',
@@ -6,7 +8,11 @@ PROXIES = {
 #请求头
 HEADERS = {
     'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.162 Safari/537.36',
-    'Cookie':'xxx'
+    'Cookie':'_ga=GA1.2.130903612.1524213531; rxx=33ovhav8kcs.13fes53n&v=1; __unam=b3ca5e5-162e234fed3-532955b8-2; yx=5zewc25q7yshg%26o%3D3%26f%3D78; __utmc=189990958; nts=false; capture=GM9WbaurZ9GpagyMInEYgOqukg; __utmz=189990958.1533880678.7.6.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); tmgioct=5b73963143b6530237443910; _gid=GA1.2.1499411357.1534497445; devicePixelRatio=2; documentWidth=1280; __gads=ID=c00dae29106b9b74:T=1534497446:S=ALNI_MZStqQVb3ayqkLRQI2SeJBI0qAmYQ; __utma=189990958.130903612.1524213531.1534497445.1534500169.12; __utmb=189990958.0.10.1534500169; pfl=ZDc5Zjk1NDA1ZjVjMWM0MzNmZTYyYjgyZGFhZGQyMjg0ZTcxMmQwOTkxOWQwNjc2MWJlOTQ5MTliZDg1ZTcwOSxuMnozODZoODR2YzJjOWFodXhlOHc1bXQyNjJzYzZrYSwxNTM0NTAwMTgw; pfp=dgrRXBqhvGG1RQianriVfblXIVPw2zch6QVo2HVE; pfs=adWBNOTdf480x49hVTNaoS5V2XU; pfe=1542276193; pfu=336713838; pfx=b8ed0ba100fb784e301dc2d1b4e384526b648f030a1a2d5bbf11abb09b94b9c2%230%233029088130; language=%2Czh_CN; logged_in=1; pfg=1456f80aee37ca180d41d5ff9384fa1952c7c04875b50693f3ddb6109663b2d9%23%7B%22gdpr_is_acceptable_age%22%3A1%2C%22exp%22%3A1566036205%2C%22vc%22%3A%22%22%7D%230655655001'
 }
+
+#存储图片路径
+DIR_PATH = os.path.join(os.path.abspath('.'), 'images')
+
 #请求页数范围（0....n)
-PAGE_RANG = range(0, 3)
+PAGE_RANG = range(0, 50)
--- tumblr.py
+++ tumblr.py
@@ -1,4 +1,3 @@
-
 from multiprocessing import pool
 from hashlib import md5
 from config import *
@@ -6,7 +5,7 @@ from bs4 import BeautifulSoup
 import re, os, time
 import json
 import requests
-
+import threading
 
 #存储图片
 def save_image(content, url):
@@ -17,7 +16,6 @@ def save_image(content, url):
 
 #下载图片
 def download_image(url):
-    time.sleep(0.1)
     print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), '下载成功-->',url)
     try:
         requests.adapters.DEFAULT_RETRIES = 5
@@ -25,17 +23,13 @@ def download_image(url):
         if respones.status_code == 200:
             #存图片
             save_image(respones.content ,url)
-            return respones.content
         else:
             print('图片下载失败')
-            return None
     except requests.exceptions.ConnectionError:
         print('图片下载失败')
-        return None
 
 #获取筛选数据
 def get_home(page):
-    time.sleep(0.2)
     url = 'https://www.tumblr.com/dashboard/'+str(page)
     try:
         r = requests.get(url, headers = HEADERS ,proxies=PROXIES)
@@ -47,33 +41,31 @@ def get_home(page):
             photoSetUrl = [item['href'] for item in photoSet] 
             
             print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), '正在下载第{}页'.format(page))
+            
             for imgUrl in photoSetUrl+photoUrl:
-                #下载图片
-                download_image(imgUrl)
-                #存数据
-                #save_to_mongo({'url':imgUrl})
-            return photoSetUrl+photoUrl
-        return None
+                #多线程下载图片
+                t = threading.Thread(target=download_image, args=(imgUrl,))
+                t.start()
+        else:
+            print('列表请求失败')
     except requests.exceptions.ConnectionError:
         print('列表请求失败')
-        return None
 
-def main(page):
-    get_home(page)
-
-if __name__ == '__main__':
+def start(page):
     print('开始--------->>')
-    #存储图片路径
-    DIR_PATH = os.path.join(os.path.abspath('.'), 'images')
+
     if not os.path.exists(DIR_PATH):
         os.mkdir(DIR_PATH)
     start_time = time.time()
     start_file_count = len([x for x in os.listdir(DIR_PATH)])
     #多进程
     p = pool.Pool()
-    p.map(main ,list(PAGE_RANG))
+    p.map(get_home ,list(PAGE_RANG))
     p.close()
     p.join()
     end_time = time.time()
     end_file_count = len([x for x in os.listdir(DIR_PATH)])
     print('结束--------->>','耗时{}s'.format(int(end_time - start_time)),'保存{}个'.format(end_file_count-start_file_count),'总共{}个'.format(end_file_count))
+
+if __name__ == '__main__':
+    start(PAGE_RANG)
