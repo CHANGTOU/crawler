49c2a2f0f004e61c9577c7677a862b4589617ff5
goose <goose>
2019-04-12 09:09:43 +0800

    修复错误
    修复错误

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
+# coding: utf-8
 import os
 
-#爬取页数范围（0....n)
-PAGE_RANG = range(0, 100)
+#爬取页数
+PAGE_COUNT = 10
 
 #设置请求头信息，更换自己的Cookie
 HEADERS = {
     'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.162 Safari/537.36',
-    'Cookie':'自得的Cookie'
+    'Cookie':'你的Cookie'
 }
 
 #爬取图片存储路径
@@ -14,6 +15,6 @@ DIR_PATH = os.path.join(os.path.abspath('.'), 'images')
 
 #设置代理http 或者 socket
 PROXIES = {
-  "http": "http://127.0.0.1:1087",
-  "https": "http://127.0.0.1:1087",
+  "http": "http://127.0.0.1:7890",
+  "https": "http://127.0.0.1:7890",
 }
--- tumblr.py
+++ tumblr.py
@@ -1,4 +1,4 @@
-from multiprocessing import pool
+# coding: utf-8
 from hashlib import md5
 from config import *
 from bs4 import BeautifulSoup
@@ -7,67 +7,78 @@ import json
 import requests
 import threading
 
+#第一个页面
+first_page_link = 'https://www.tumblr.com'
+#下一个页面
+next_page_link = '/dashboard/0'
+
 #存储图片
 def save_image(content, url):
-    file_path = '{}/{}{}'.format(DIR_PATH, md5(content).hexdigest(), '.'+url.split('.')[-1])
-    if not os.path.exists(file_path):
-        with open(file_path, 'wb') as file:
-            file.write(content)
-
+	file_path = '{}/{}{}'.format(DIR_PATH, md5(content).hexdigest(), '.'+url.split('.')[-1])
+	if not os.path.exists(file_path):
+		with open(file_path, 'wb') as file:
+			file.write(content)
+			
 #下载图片
 def download_image(url):
-    print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), '下载成功-->',url)
-    try:
-        requests.adapters.DEFAULT_RETRIES = 5
-        respones = requests.get(url ,timeout = 20 ,proxies=PROXIES)
-        if respones.status_code == 200:
-            #存图片
-            save_image(respones.content ,url)
-        else:
-            print('图片下载失败')
-    except requests.exceptions.ConnectionError as error:
-        print('图片失败：' + str(error))
-
+	try:
+		requests.adapters.DEFAULT_RETRIES = 5
+		respones = requests.get(url ,timeout = 20 ,proxies=PROXIES)
+		if respones.status_code == 200:
+			print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), '\n图片下载成功-->',url)
+			#存图片
+			save_image(respones.content ,url)
+		else:
+			print('图片下载失败')
+	except requests.exceptions.ConnectionError as error:
+		print('图片失败：' + str(error))
+		
 #获取筛选数据
-def get_home(page):
-    url = 'https://www.tumblr.com/dashboard/'+str(page)
-    try:
-        r = requests.get(url, headers = HEADERS ,proxies=PROXIES)
-        if r.status_code == 200:
-            try:
-                soup = BeautifulSoup(r.text ,'lxml')
-                photo = soup.select('.post_media_photo')
-                photoUrl = [item['src'] for item in photo]
-                photoSet = soup.select('.photoset_photo')
-                photoSetUrl = [item['href'] for item in photoSet]
-                print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), '正在下载第{}页'.format(page))
-                for imgUrl in photoSetUrl+photoUrl:
-                    #多线程下载图片
-                    t = threading.Thread(target=download_image, args=(imgUrl,))
-                    t.start()
-            except BaseException as e:
-                print('解析错误：' + str(e))
-        else:
-            print('列表请求失败')
-    except requests.exceptions.ConnectionError as error:
-        print('列表失败：' + str(error))
-
+def get_page_image(url):
+	print(url)
+	try:
+		r = requests.get(url, headers = HEADERS ,proxies=PROXIES)
+		if r.status_code == 200:
+			try:
+				soup = BeautifulSoup(r.text, "lxml")
+				# 下一页链接
+				next_page = soup.select_one('#next_page_link')
+				global next_page_link
+				next_page_link = next_page['href']
+				# 单张图片
+				photo = soup.select('.post_media_photo')
+				photo_urls = [item['src'] for item in photo]
+				# 集合图片
+				photoSet = soup.select(".photoset_photo rapid-noclick-resp")
+				photoSet_urls = [item['href'] for item in photoSet]
+				for url in photo_urls + photoSet_urls:
+					#多线程下载图片
+					t = threading.Thread(target=download_image, args=(url,))
+					t.start()
+			except BaseException as e:
+				print('解析错误：' + str(e))
+		else:
+			print('列表请求失败')
+	except requests.exceptions.ConnectionError as error:
+		print('列表失败：' + str(error))
+		
 def start(page):
-    print('开始--------->>')
+	print('开始--------->>')
+	
+	if not os.path.exists(DIR_PATH):
+		os.mkdir(DIR_PATH)
+	start_time = time.time()
+	start_file_count = len([x for x in os.listdir(DIR_PATH)])
 
-    if not os.path.exists(DIR_PATH):
-        os.mkdir(DIR_PATH)
-    start_time = time.time()
-    start_file_count = len([x for x in os.listdir(DIR_PATH)])
-    #多进程
-    p = pool.Pool()
-    p.map(get_home ,list(PAGE_RANG))
-    p.close()
-    p.join()
-    end_time = time.time()
-    end_file_count = len([x for x in os.listdir(DIR_PATH)])
+	for page in list(range(page)):
+		print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), '\n下载第{}页图片'.format(page))
+		get_page_image(first_page_link + next_page_link)
 
-    print('结束--------->>','耗时{}s'.format(int(end_time - start_time)),'保存{}个'.format(end_file_count-start_file_count),'总共{}个'.format(end_file_count))
-
+	end_time = time.time()
+	end_file_count = len([x for x in os.listdir(DIR_PATH)])
+	
+	print('结束--------->>','耗时{}s'.format(int(end_time - start_time)),'保存{}个'.format(end_file_count-start_file_count),'总共{}个'.format(end_file_count))
+	
 if __name__ == '__main__':
-    start(PAGE_RANG)
+	start(PAGE_COUNT)
+
